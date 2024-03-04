597443bd95da8db2a4700dc29e535786a9e5b9de
MaximilianMcC <max@maximilian.co.nz>
2024-03-04 15:09:33 +1300

    begin map

--- src/Map.cs
+++ src/Map.cs
@@ -1,11 +1,31 @@
 using System.Numerics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Raylib_cs;
 
 class Map
 {
 	public static void LoadMap(string filePath)
 	{
-		// Load in all of the tiles
+		// Open the tiles json file
+		string json = File.ReadAllText("./maps/tiles.json");
+		Tile[] tiles = JsonSerializer.Deserialize<Tile[]>(json);
+
+
+		// Open the map file
+		string[] mapLines = File.ReadAllLines(filePath);
+
+		
+		// Open the map then loop through
+		// every tile in the map
+		// TODO: Don't use nested foreach
+		foreach (string line in mapLines)
+		{
+			foreach (char identifier in line.ToCharArray())
+			{
+				// Check for if the 
+			}
+		}
 	}
 
 
@@ -19,14 +39,27 @@ class Map
 	{
 
 	}
+
+
+
+
 }
 
 
 class Tile
 {
+	[JsonPropertyName("identifier")]
 	public char Identifier { get; set; }
+
+	[JsonPropertyName("name")]
 	public string Name { get; set; }
+
+	[JsonPropertyName("texturePath")]
 	public string TexturePath { get; set; }
+
+	[JsonPropertyName("frictionMultiplier")]
 	public float FrictionMultiplier { get; set; }
+
+	[JsonPropertyName("collision")]
 	public bool Collision { get; set; }
 }
