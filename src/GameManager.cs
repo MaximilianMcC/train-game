07faf7d7d723b235d006691fb6685ddbd3c4b2f1
MaximilianMcC <max@maximilian.co.nz>
2024-06-03 21:31:38 +1200

    Began working on day/night ui (ugly)

--- src/GameManager.cs
+++ src/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Raylib_cs;
 
 class GameManager
@@ -5,7 +6,7 @@ class GameManager
 	// Day stuff
 	// TODO: Put in another file/class
 	private static float dayLength = 25; //? 1 minute 60
-	private static float nightLength = 10; //? quick as 30
+	private static float nightLength = 15; //? quick as 30
 	private static float totalCycleLength = dayLength + nightLength;
 	private static float currentTime = 0;
 
@@ -30,6 +31,57 @@ class GameManager
  		float remainingTime = (timeString == "Day") ? dayLength - currentTime : nightLength - (currentTime - dayLength);
         float percentage = (remainingTime / ((timeString == "Day") ? dayLength : nightLength)) * 100f;
 		Raylib.DrawText($"Time: {currentTime.ToString("F1")} ({timeString})\n\n{percentage.ToString("F")}%", 100, 100, 30, Color.White);
+
+
+		// Time thingy
+		// TODO: No magic numbers
+		float width = Game.Size.X - 200f;
+		float height = 25f;
+		float x = 100f;
+		float y = Game.Size.Y - 95f;
+		Raylib.DrawRectangleRec(new Rectangle(x, y, width, height), Color.Gold);
+
+		// Cheeky save the initial x for returning to draw
+		// the cursor thingy later on
+		float initialX = x;
+
+		// Define the colors for the times
+		Color nightBackground = Color.Black;
+		Color dayBackground = Color.SkyBlue;
+
+		// Calculate the scales of the sections
+		float nightScale = (nightLength / totalCycleLength) * width;
+		float dayScale = (dayLength / totalCycleLength) * width;
+
+		// Calculate the size for the day
+		float dayWidth = (dayLength / totalCycleLength) * width;
+
+		// Calculate the sizes for the full night, and
+		// the transition between night and day
+		//? Halved because half of it on left, half on right
+		float nightWidth = ((nightLength / totalCycleLength) * width) / 2;
+		float transitionWidth = dayWidth / 10f;
+		nightWidth -= transitionWidth;
+
+		// Draw the left night and transition
+		Raylib.DrawRectangleRec(new Rectangle(x, y, nightWidth, height), nightBackground);
+		x += nightWidth;
+		Raylib.DrawRectangleGradientEx(new Rectangle(x, y, transitionWidth, height), nightBackground, nightBackground, dayBackground, dayBackground);
+		x += transitionWidth;
+
+		// Draw the day
+		Raylib.DrawRectangleRec(new Rectangle(x, y, dayWidth, height), dayBackground);
+		x += dayWidth;
+
+		// Draw the transition and right night
+		Raylib.DrawRectangleGradientEx(new Rectangle(x, y, transitionWidth, height), dayBackground, dayBackground, nightBackground, nightBackground);
+		x += transitionWidth;
+		Raylib.DrawRectangleRec(new Rectangle(x, y, nightWidth, height), nightBackground);
+
+		// Draw the little pointer cursor arrow thingy that
+		// shows what the current time is
+		x = initialX + (currentTime / totalCycleLength) * width;
+		Raylib.DrawLineEx(new Vector2(x, y), new Vector2(x, height), 5f, Color.Beige);
 	}
 
 	public static void CleanUp()
