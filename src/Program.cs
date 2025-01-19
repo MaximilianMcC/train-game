5dc4309b1f230be9a9cd356310d852755d218e2e
MaximilianMcC <max@maximilian.co.nz>
2025-01-20 00:28:03 +1300

    Thing can travel on spline at constant speed

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,9 @@ using Raylib_cs;
 
 class Program
 {
+	private static Spline spline;
+	private static float testPosition = 0;
+
 	public static void Main(string[] args)
 	{
 		// Raylib stuff
@@ -29,14 +32,25 @@ class Program
 
 	private static void Start()
 	{
+        spline = new Spline(
+			new Vector2(0, 0) * 100,
+			new Vector2(1, 2) * 100,
+			new Vector2(3, 3) * 100,
+			new Vector2(4, 0) * 100
+		);
 	}
 
 	private static void Update()
 	{
+		testPosition += 0.2f * Raylib.GetFrameTime();
+		if (testPosition > 1) testPosition = 0;
 	}
 
 	private static void Render()
 	{
+		// spline.DrawSpline(Vector2.Zero, 5f, Color.White);
+		spline.Draw(new Vector2(100, 100), 5f, Color.Red, 200);
+		Raylib.DrawCircleV(new Vector2(100, 100) + spline.GetPosition(testPosition), 6f, Color.Pink);
 	}
 
 	private static void CleanUp()
--- src/Util/Spline.cs
+++ src/Util/Spline.cs
@@ -1,53 +1,118 @@
 using System.Numerics;
+using Raylib_cs;
 
 class Spline
 {
-	public Vector2 Point1 { get; set; }
-	public Vector2 Point2 { get; set; }
-	public Vector2 Point3 { get; set; }
-	public Vector2 Point4 { get; set; }
+	private Vector2 point1;
+	private Vector2 point2;
+	private Vector2 point3;
+	private Vector2 point4;
+	private float[] arcLengths;
 
 	// Catmull rom spline or something idk
-	public Spline(Vector2 point1, Vector2 point2, Vector2 point3, Vector2 point4)
+	public Spline(Vector2 point1, Vector2 point2, Vector2 point3, Vector2 point4, int samples = 100)
 	{
 		// Assign the points
-		Point1 = point1;
- 		Point2 = point2;
- 		Point3 = point3;
- 		Point4 = point4;
+		this.point1 = point1;
+ 		this.point2 = point2;
+ 		this.point3 = point3;
+ 		this.point4 = point4;
+
+		// Pre-calculate the arc lengths
+		arcLengths = ComputeArcLengthTable(samples);
 	}
 
-	// Represents the position on the line and is from 0.0 - 1.0
-	public Vector2 GetPositionAtPercentage(float percentageThroughLine)
+	// Catmull rom spline from another
+	//? Used for if they wanna edit this one or something
+	// TODO: Don't do this
+	public Spline(Spline spline, Vector2 point1Adjustment, Vector2 point2Adjustment, Vector2 point3Adjustment, Vector2 point4Adjustment)
 	{
-		return new Vector2(
-			GetPositionComponent(true, percentageThroughLine),
-			GetPositionComponent(false, percentageThroughLine)
-		);
+		// Update the adjustments
+		point1 = spline.point1 + point1Adjustment;
+		point2 = spline.point2 + point2Adjustment;
+		point3 = spline.point3 + point3Adjustment;
+		point4 = spline.point4 + point4Adjustment;
+
+		// Pre-calculate the arc lengths again
+		arcLengths = ComputeArcLengthTable(100);
 	}
 
-	//! idk what any of this does. Shout out best friend
-	private float GetPositionComponent(bool x, float percentage)
+	// Get the position of a point at the percentage through
+	private Vector2 GetPositionAtPercentage(float percentageThroughLine)
 	{
-		// Get like positions through the spline?
-		float t = percentage;
+		//! idk what any of this does. Best friend locked in
+		float t = percentageThroughLine;
 		float t2 = t * t;
-    	float t3 = t2 * t;
+		float t3 = t2 * t;
 
-		// Figure out what component we're using
-		//? x, or y
-		float p0 = x ? Point1.X : Point1.Y;
-		float p1 = x ? Point2.X : Point2.Y;
-		float p2 = x ? Point3.X : Point3.Y;
-		float p3 = x ? Point4.X : Point4.Y;
+		float a0 = -0.5f * t3 + t2 - 0.5f * t;
+		float a1 = 1.5f * t3 - 2.5f * t2 + 1.0f;
+		float a2 = -1.5f * t3 + 2.0f * t2 + 0.5f * t;
+		float a3 = 0.5f * t3 - 0.5f * t2;
 
-		// Phat bit of maths from the best friend
-		float value = 0.5f * ((2 * p1) +
-					(-p0 + p2) * t +
-					(2 * p0 - 5 * p1 + 4 * p2 - p3) * t2 +
-					(-p0 + 3 * p1 - 3 * p2 + p3) * t3);
-
-		// Chuck it back
-		return value;
+		return a0 * point1 + a1 * point2 + a2 * point3 + a3 * point4;
 	}
+
+	// Pre calculate the arc length lookup table
+	private float[] ComputeArcLengthTable(int samples)
+	{
+		//! from the best friend. idk what this does
+		float[] arcLengths = new float[samples];
+		arcLengths[0] = 0;
+		Vector2 previousPoint = GetPositionAtPercentage(0);
+
+		for (int i = 1; i < samples; i++)
+		{
+			float t = i / (float)(samples - 1);
+			Vector2 currentPoint = GetPositionAtPercentage(t);
+			arcLengths[i] = arcLengths[i - 1] + Vector2.Distance(previousPoint, currentPoint);
+			previousPoint = currentPoint;
+		}
+
+		return arcLengths;
+	}
+
+	// Get the position evenly spaced
+	private float GetNormalizedTForArcLength(float distance)
+	{
+		//! from the best friend. idk what this does
+		for (int i = 1; i < arcLengths.Length; i++)
+		{
+			if (distance <= arcLengths[i])
+			{
+				float segmentLength = arcLengths[i] - arcLengths[i - 1];
+				float segmentT = (distance - arcLengths[i - 1]) / segmentLength;
+				return (i - 1 + segmentT) / (arcLengths.Length - 1);
+			}
+		}
+
+		return 1;
+	}
+
+	public Vector2 GetPosition(float normalizedDistance)
+	{
+		float t = GetNormalizedTForArcLength(normalizedDistance * arcLengths[^1]);
+		return GetPositionAtPercentage(t);
+	}
+
+	// Draw the current spline
+	public void Draw(Vector2 position, float size, Color color, int pointCount = 100)
+	{
+		// Figure out how much we gotta increase per
+		// iteration because the percentage goes from
+		// a number 0-1
+		float incrementor = 1f / pointCount;
+
+		// Loop through x times and draw a dot to make the line
+		for (float t = 0; t < 1; t += incrementor)
+		{
+			// Get the position of the current point
+			Vector2 currentPointPosition = GetPositionAtPercentage(t);
+			Vector2 screenPosition = position + currentPointPosition;
+
+			// Draw it
+			Raylib.DrawCircleV(screenPosition, size, color);
+		}
+	}
+
 }
